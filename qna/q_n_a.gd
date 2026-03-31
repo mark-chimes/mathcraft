6c910ca48285d138cdc811f6602a49cfcf10f394
Mark Chimes <chimes.mark@gmail.com>
2026-03-31 23:43:15 +0200

    Basic question generator

--- qna/q_n_a.gd
+++ qna/q_n_a.gd
@@ -1,7 +1,31 @@
 extends Node2D
 
 @export var question_generator: QuestionGenerator
+var current_question : QuestionData
 
 func _ready(): 
-	var current_question : QuestionData = question_generator.generate()
+	generate_question()
+	
+func generate_question(): 
+	if question_generator == null: 
+		printerr("QnA Question Generator is null")
+	current_question = question_generator.generate()
 	$QuestionDisplay.display_question(current_question)
+
+func _process(delta) -> void: 
+	# only goes up to 9
+	for x in range(0,10): 
+		if Input.is_action_just_pressed("num" + str(x)):
+			press_num(x)
+	
+func press_num(num):
+	if current_question.answer_type != QuestionData.AnswerType.INTEGER:
+		return
+	print("Pressed number: " + str(num))
+	
+	if num == current_question.correct_answer:
+		print("SUCCESS!")
+		generate_question()
+	else: 
+		print("Failure")
+	
