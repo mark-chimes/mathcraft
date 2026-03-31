608df9425aa07fb474148018253aff66584cae99
Mark Chimes <chimes.mark@gmail.com>
2026-03-31 17:09:12 +0200

    Stock display with controls.

--- stock/stock_display.gd
+++ stock/stock_display.gd
@@ -1,14 +1,29 @@
+@tool
 extends Control
 
 @export var item_display_scene: PackedScene
+@export var items: Array[ItemData] = []
 
 @onready var container = $StockContainer
 
 var displays := {} # Dictionary<ItemData, Node>
 
-func display_inventory(items: Array[ItemData]):
+func _enter_tree():
+	if Engine.is_editor_hint():
+		_update_display()
+
+func _update_display():
+	for child in container.get_children():
+		child.queue_free()
+	displays.clear()
+
 	for item in items:
-		var display = item_display_scene.instantiate()
-		display.setup(item)
-		container.add_child(display)
-		displays[item] = display
+		if item_display_scene:
+			var display = item_display_scene.instantiate()
+			display.setup(item)
+			container.add_child(display)
+			displays[item] = display
+
+	#if Engine.is_editor_hint():
+		#container.queue_sort()   # updates container layout
+		#container.update()       # redraws Control nodes
--- stock/test/stocktest.gd
+++ stock/test/stocktest.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
-@export var all_items: Array[ItemData]
+@export var all_items: Array[ItemData] = []
 
 func _ready():
-	$StockDisplay.display_inventory(all_items)
+	$StockDisplay.items = all_items
+	# optionally call update if you want
+	if $StockDisplay.has_method("_update_display"):
+		$StockDisplay._update_display()
